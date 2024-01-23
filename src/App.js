942fef9cfa5586be297209788ecefa94e0e707ac
Phili0916 <79313259+Phili0916@users.noreply.github.com>
2024-01-23 11:28:25 +0100

    fetched store data

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 const App = () => {
+  // https://fakestoreapi.com/products
+  const [apiData, setApiData] = useState([])
+
+  useEffect(()=> {
+    apiStoreData()
+  },[])
+  const apiStoreData = async () => {
+    const response = await fetch("https://fakestoreapi.com/products")
+    // console.log(response)
+    const data = await response.json()
+    // console.log(data)
+    setApiData(data)
+  }
   return (
     <>
       <h2>API Store Project</h2>
       <div className='container'>
-
+        {apiData.map((values)=>{
+          return(
+            <>
+              <div className='box'>
+                <div className='content'>
+                  <h5>{values.title}</h5>
+                  <p>{values.description}</p>
+                </div>
+                <img src={values.image} alt="product image"/>
+              </div>
+            </>
+          )
+        })}
       </div>
     </>
   )
